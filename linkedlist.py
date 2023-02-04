38a132ef37cc4da1dc43ffbe6f069b0b6301edf0
mojo <sabanajholo@gmail.com>
2023-02-04 13:48:43 +0800

    Utilized added linkedlist

--- linkedlist.py
+++ linkedlist.py
@@ -1,3 +1,24 @@
-class linkedlist:
-  def __init__(self):
-    self.head = None
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def add(self, node: Node):
+        if self.head == None:
+            self.head = node
+        else:
+            cur = self.head
+            while cur.next != None:
+                cur = cur.next
+            cur.next = node
+
+    def display(self):
+        cur = self.head
+        while cur != None:
+            print(cur.val, end=" -> ")
+            cur = cur.next
+        print(cur)
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("first commit")
+from linkedlist import *
+
+def main():
+    list1 = LinkedList()
+    for i in range(1, 6):
+        list1.add(Node(i))
+    list1.display()
+
+main()
